58a55fbe257b93a57d3b9cb326916eaacc184231
Noelia <116829503+noelia1522@users.noreply.github.com>
2023-01-17 14:58:51 +0100

    updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,20 @@
-const fs=  require("fs");
+const fs = require("fs");
 const _ = require("lodash");
-const authModel= require("../models/authModel");
+const authModel = require("../models/authModel");
 const { BadRequest } = require("../utils/errors");
 
 
 ///////////// Register User ///////////////////////
-async function RegisterUser(req,res,next){
-    const {name,email,password} = req.body;
+async function RegisterUser(req, res, next) {
+    const { name, email, password } = req.body;
 
-    try{
-        if(!name ||!email || ! password){
+    try {
+        if (!name || !email || !password) {
             throw new BadRequest("Missing required field!")
-        }else{
-            const registeredUser= await authModel.create({
-                name:name,
-                email:email,
+        } else {
+            const registeredUser = await authModel.create({
+                name: name,
+                email: email,
                 password: hashedPassword
             })
             console.log(registeredUser)
@@ -22,7 +22,7 @@ async function RegisterUser(req,res,next){
             res.redirect("/");
         }
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 }
@@ -30,6 +30,40 @@ async function RegisterUser(req,res,next){
 
 /////////////////// login user ////////////
 
+async function loginUser(req, res,next) {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            throw new BadRequest("Missing required field!")
+        } else {
+            passport.authenticate("local", {
+                successRedirect: '/',
+                failureRedirect: '/login', //if email is repetetive or passwordi s wrong
+                failureFlash: true
+            })
+        }
+    }
+    catch(err){
+        next(err)
+    }
+
+}
+
+////////////// LOGOUT ///////////////////
+async function logoutUser(req,res,next){
+    req.logOut();
+    res.clearCookie("connect.sid", { doamin: "localhost", path: "/" });
+  
+    //logged out and redirect to login
+  
+    req.session.destroy(function (error) {
+      if (error) {
+        return next(error)//check it later
+      }
+      res.redirect('/login');
+    })
+  }
 
 
+module.exports = { RegisterUser, loginUser, logoutUser }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const initializePassport = require('../config/passport-config')
 //we import the config file 
 const User = require("../models/user");
+const { RegisterUser, loginUser, logoutUser } = require('../controllers/authController');
 
 
 initializePassport(passport)
@@ -21,23 +22,9 @@ router.get('/', checkAuthenticated, (req, res) => {
   router.get('/login', checkNotAuthenticated, (req, res) => {//if the user is logged in, she shouldn't see the login page
     res.render('login.ejs')//this function is implimented
   })
-  router.get('/login', checkNotAuthenticated, (req, res) => { //if the user is logged in, she shouldn't be able to see the login page
-    res.render('login.ejs') //this function is implemented
-  })
+
   
-  router.post('/login', checkNotAuthenticated, passport.authenticate("local", {
-    successRedirect: '/',
-    failureRedirect: '/login', //if email is repetetive or passwordi s wrong
-    failureFlash: true
-  
-  })
-  )
-  
-  router.post('/login', checkNotAuthenticated, passport.authenticate("local", {
-    successRedirect: '/',
-    failureRedirect: '/login',//if email is repetitive or password wrong
-    failureFlash: true
-  }))
+  router.post('/login', checkNotAuthenticated,loginUser)
   
   
   //////////////// REGISTER PAGE /////////////////////
@@ -46,46 +33,11 @@ router.get('/', checkAuthenticated, (req, res) => {
     res.render('register.ejs')
   })
   
-  router.post('/register', checkNotAuthenticated, async (req, res) => {
-  
-    const { name, email, password } = req.body;
-    //TODO: check if email already exists!!
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);//salt = 10 
-    //it has to wait for the password bc it's interaction with a database!
-    try {
-      if (!name || !password || !email) {
-        throw new BadRequest("Missing required field: name, password or email")
-      }
-      await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      });
-      //register and redirect to login
-      res.redirect('/login')
-    }
-    catch (err) {
-      console.log(err);
-      res.redirect('/register');
-    }
-  })
-
+  router.post('/register', checkNotAuthenticated,RegisterUser)
 
   //////////// LOGOUT ////////////////
   
-  router.delete('/logout', (req, res) => { //delete the session id
-    req.logOut();
-    res.clearCookie("connect.sid", { doamin: "localhost", path: "/" });
-  
-    //logged out and redirect to login
-  
-    req.session.destroy(function (error) {
-      if (error) {
-        return next(error)//check it later
-      }
-      res.redirect('/login');
-    })
-  })
+  router.delete('/logout',logoutUser)
   
   
   //helper middleware
